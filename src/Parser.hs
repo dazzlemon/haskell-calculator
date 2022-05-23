b44026a9b0a9281651b39f09a69c58af04821a65
dazzlemon <daanill13470@gmail.com>
2022-05-23 19:46:11 +0300

    finished one branch

--- src/Parser.hs
+++ src/Parser.hs
@@ -22,6 +22,8 @@ operatorPrecedence x
 	| x `elem` [Multiply, Divide, Power] = 1
 	| x `elem` [Plus, Minus] = 2
 
+parse :: [TokenInfo] -> Either CalculatorError Expression -- TODO
+
 getExpression :: [TokenInfo] -> Either CalculatorError (Expression, [TokenInfo])
 getExpression [] = Right (EmptyExpression, [])
 getExpression [TokenInfo pos single] = case single of
@@ -30,23 +32,29 @@ getExpression [TokenInfo pos single] = case single of
 getExpression ((TokenInfo pos first):rest) = case first of
 	TokenNumber number -> case head rest of
 		TokenInfo _ (FactorialOperator fOp) -> Right (Factorial number, tail rest)
-		TokenInfo _ (TokenOperator tOp) -> if top `elem` [Plus, Minus]
-			then -- TODO: lookahead
-			else case getExpression $ tail rest of
+		TokenInfo _ (TokenOperator tOp) -> if tOp `elem` [Plus, Minus]
+			then case getExpression $ tail rest of
 				(s@(SimpleExpression _), rest')
 					-> Right (OperatorCall number tOp s, rest')
-				(p@(ParenthesisExpression _), rest') 
-					-> Right (OperatorCall number tOp p, rest')
-				(fc@(FunctionCall _ _), rest')
-					-> Right (OperatorCall number tOp fc, rest')
+				(oc@(OperatorCall e1 o e2), rest') -- change precedence
+					-> if operatorPrecedence o > operatorPrecedence tOp
+							then Right (OperatorCall number tOp oc, rest')
+							else Right (OperatorCall (OperatorCall number tOp e1) o e2, rest')
+				EmptyExpression -> Left $ CalculatorParserError $ ParserUnexpectedEOF
+				_ -> case parse rest' of -- this is possible because
+				                         -- we have only two levels of precedence
+					err@(Left _) -> err
+					Rigth expression -> Right (OperatorCall number tOp expression, [])
+			else case getExpression $ tail rest of
 				((OperatorCall e1 o e2), rest') -- change precedence to be left to right
 					-> Right (OperatorCall (OperatorCall number tOp e1) o e2, rest')
-				(f@(Factorial _), rest')
-					-> Right (OperatorCall number tOp f, rest')
+				(expression, rest')
+					-> Right (OperatorCall number tOp expression, rest')
 				EmptyExpression -> Left $ CalculatorParserError $ ParserUnexpectedEOF
 		TokenInfo pos _ -> unexpectedToken pos
-	TokenFunction Function -> -- TODO:
-  ParenthesisLeft -> -- TODO:
+	TokenFunction Function ->
+		-- TODO: take Comma and Expression until hit ParenthesisRight
+  ParenthesisLeft -> -- TODO: getExpression until hit ParenthesisRight
 	_ -> unexpectedToken pos
 
 unexpectedToken pos = Left $ CalculatorParserError $ UnexpectedToken pos
